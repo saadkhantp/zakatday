be89bccd587437fb142ba52f0f7cdd064db49cfa
saadkhantp <saadkhan.tp@outlook.com>
2024-03-16 01:23:11 +0500

    refactor, remove extra heading

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ document
   .getElementById("zakatForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
-    // Get selected currency
     const currency = document.querySelector(
       'input[name="currency"]:checked'
     ).value;
@@ -18,16 +16,13 @@ document
     );
     const zakatRate = 0.025; // 2.5%
 
-    // Get the Nisab value for the selected currency
     const nisab = nisabValues[currency];
 
-    let message; // This will store the message to be displayed to the user
+    let message;
 
     if (totalWealth <= nisab) {
-      // If total wealth is equal to or less than the Nisab
-      message = `<h3>You are not eligible for Zakat</h3><p>Your assets have not reached the Nisab (minimum amount) required for Zakat in ${currency}.</p>`;
+      message = `<p>Your assets have not reached the Nisab (minimum amount) required for Zakat in ${currency}.</p>`;
     } else {
-      // If total wealth is above the Nisab, calculate Zakat
       const zakatDue = totalWealth * zakatRate;
       message = `<h3>Your due Zakat is<br/><span>${currency} ${zakatDue.toFixed(
         2
